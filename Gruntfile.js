e88697648251822970fd4694afbac15848f26848
nathan <nathanzankich@gmail.com>
2014-07-07 20:06:37 -0700

    add IndexCtrl test

---
+++ Gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		karma: {
+            unit: {
+                configFile: 'karma.conf.js'
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-karma');
+
+    grunt.registerTask('devmode', ['karma:unit', 'watch']);
+};
---
+++ test/main.js
@@ -0,0 +1,31 @@
+describe('IndexCtrl', function() {
+    var $scope, $rootScope, $httpBackend, $timeout, testController;
+    beforeEach(inject(function($injector) {
+        $timeout = $injector.get('$timeout');
+        $httpBackend = $injector.get('$httpBackend');
+        $rootScope = $injector.get('$rootScope');
+        $scope = $rootScope.$new();
+
+        var $controller = $injector.get('$controller');
+
+        testController = function() {
+            return $controller('IndexCtrl', {
+                '$scope': $scope
+            });
+        };
+    }));
+
+    afterEach(function() {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('IndexCtrl should get array of robots', function() {
+        var controller = testController();
+        jasmine.getJSONFixtures().fixturesPath='base/test';
+        var data = loadJSONFixtures('robots.json');
+        $httpBackend.expect('GET', '/robots').respond(data);
+        $httpBackend.flush();
+        expect($scope.robots).toEqual(data);
+    });
+});
