817779c428134c08bc47a198f8e3be531144a082
nathan <nathanzankich@gmail.com>
2014-07-11 17:52:47 -0700

    add tests for device and robot commands

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
 
       {pattern: 'test/support/robots.json', watched: true, served: true, included: false},
       {pattern: 'test/support/myRobot.json', watched: true, served: true, included: false},
-      {pattern: 'test/support/robotArray.json', watched: true, served: true, included: false}
+      {pattern: 'test/support/myDevice.json', watched: true, served: true, included: false}
     ],
 
 
