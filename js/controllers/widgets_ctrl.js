750d8aaef2f6946e63c0a2a61363e17f200f4309
Andrew Stewart <andrew@stwrt.ca>
2014-08-29 10:54:39 -0700

    Add widget persistence

--- js/controllers/widgets_ctrl.js
+++ js/controllers/widgets_ctrl.js
@@ -1,7 +1,22 @@
 var WidgetsCtrl = function WidgetsCtrl($scope, Widgets) {
   $scope.widgets = Widgets;
-  $scope.activeWidgets = Widgets.activeWidgets[$scope.robot.name] || [];
+  $scope.activeWidgets = [];
   $scope.newWidget = false;
+
+  // fetch robot name from parent
+  var interval = setInterval(function() {
+    if ($scope.$parent.robot) {
+      $scope.$apply(function() {
+        $scope.robot = $scope.$parent.robot.name;
+        $scope.activeWidgets = Widgets.activeWidgets[$scope.robot] || [];
+      })
+    }
+  }, 100);
+
+  var save = function() {
+    Widgets.activeWidgets[$scope.robot] = $scope.activeWidgets;
+    Widgets.save();
+  }
 
   $scope.newActiveWidget = function(widget) {
     if (widget.base === '') {
@@ -12,6 +27,14 @@ var WidgetsCtrl = function WidgetsCtrl($scope, Widgets) {
     delete widget.base;
 
     $scope.activeWidgets.push(widget);
+
+    save();
+
     $scope.newWidget = false;
-  }
+  };
+
+  $scope.removeWidget = function(i) {
+    $scope.activeWidgets.splice(i, 1);
+    save();
+  };
 };
